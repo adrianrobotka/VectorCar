3e2ae0056aac2a7792edc5ab8a2e229062c59b00
Adrian Robotka <robotka.adrian@gmail.com>
2016-11-03 22:24:24 +0100

    calculate relative screen units #18

--- app/src/main/java/hu/pendroid/dnga/vectorcar/Config.java
+++ app/src/main/java/hu/pendroid/dnga/vectorcar/Config.java
@@ -9,20 +9,17 @@ public final class Config {
      * the full abstract width of the screen
      */
     public static final float WIDTH = 100;
-
-    /**
-     * the full abstract height of the screen
-     */
-    public static final float HEIGHT = 50;
-
     /**
      * Frames per seconds
      */
     public static final float FPS = 60;
-
     /**
      * The initial speed of the ball measured in 'pixel'/frame
      */
     public static final float INITIAL_CAR_SPEED = 0.1f;
+    /**
+     * the full abstract height of the screen (calculated from the width)
+     */
+    public static float HEIGHT;
 
 }
--- app/src/main/java/hu/pendroid/dnga/vectorcar/view/GameDrawer.java
+++ app/src/main/java/hu/pendroid/dnga/vectorcar/view/GameDrawer.java
@@ -2,8 +2,6 @@ package hu.pendroid.dnga.vectorcar.view;
 
 import android.content.Context;
 import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Paint;
 import android.util.AttributeSet;
 import android.view.View;
 
@@ -16,10 +14,6 @@ import hu.pendroid.dnga.vectorcar.Config;
  * Draw game
  */
 public final class GameDrawer extends MainDrawer {
-    private static final int BORDER_WIDTH = 0;
-    private static final int INNER_PADDING = 1;
-    private Paint borderPaint;
-
     /**
      * Creates GameDrawer
      *
@@ -44,11 +38,6 @@ public final class GameDrawer extends MainDrawer {
         if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.HONEYCOMB)
             setLayerType(View.LAYER_TYPE_SOFTWARE, null);
 
-        borderPaint = new Paint();
-        borderPaint.setColor(Color.WHITE);
-        borderPaint.setStrokeWidth(BORDER_WIDTH);
-        borderPaint.setStyle(Paint.Style.STROKE);
-
         for (Drawer drawer : getDrawers()) {
             drawer.setContext(context);
         }
@@ -72,22 +61,13 @@ public final class GameDrawer extends MainDrawer {
      * @param canvas to draw on
      */
     private void preDrawers(Canvas canvas) {
-        float r1 = (float) getWidth() / (Config.WIDTH + BORDER_WIDTH + INNER_PADDING * 2);
-        float r2 = (float) getHeight() / (Config.HEIGHT + BORDER_WIDTH + INNER_PADDING * 2);
+        int screenWidth = getWidth();
+        float gameWidth = Config.WIDTH;
+        float scale = screenWidth / gameWidth;
+        canvas.scale(scale, scale);
 
-        if (r1 < r2) {
-            canvas.translate(0, (getHeight() - r1 * (Config.HEIGHT + BORDER_WIDTH + INNER_PADDING * 2)) / 2);
-            canvas.scale(r1, r1);
-        } else {
-            canvas.translate((getWidth() - r2 * (Config.WIDTH + BORDER_WIDTH + INNER_PADDING * 2)) / 2, 0);
-            canvas.scale(r2, r2);
-        }
-
-        canvas.translate((float) BORDER_WIDTH / 2 + INNER_PADDING, (float) BORDER_WIDTH / 2 + INNER_PADDING);
-
-        // Border
-        canvas.drawRect(-INNER_PADDING, -INNER_PADDING, Config.WIDTH + INNER_PADDING,
-                Config.HEIGHT + INNER_PADDING, borderPaint);
+        // Set full abstract height of the screen (calculated from the width)
+        Config.HEIGHT = Config.WIDTH * getHeight() / getWidth();
     }
 
     /**
